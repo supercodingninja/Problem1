8fdbcd8540f789d2c0abfbda9be2587b2891cfd4
Frederick Thomas <supercodingninja@outlook.com>
2017-06-25 21:52:22 -0700

    Final

--- gitHubFunctions.js
+++ gitHubFunctions.js
@@ -93,11 +93,12 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(testArray){ //eslint-disable-line
-
+  var firstElement = [a * b * c]
+    return ['a * b * c']
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(2,3,4);
+testMultiplyArray(2,3,4);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
-Contact GitHub
+// Contact GitHub
